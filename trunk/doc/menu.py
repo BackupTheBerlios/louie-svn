697364a117a458855720dab0265511c0a3f8cb1d
lazaridis_com <lazaridis_com@51480c45-b906-0410-afd9-a75b88ce8a91>
2006-02-07 11:08:11 +0000

    menu configuration file for the template pudge/orbtech.com

---
+++ trunk/doc/menu.py
@@ -0,0 +1,10 @@
+# used to generate the main menu
+
+menu_entries = [
+    ('Home', 'index.html', 'Project Home / Index'),
+    ('Doc', 'doc-index.html', 'Documentation Index'),
+    ('Modules', 'module-index.html', 'Package and Moduel Reference'),
+    ('Discuss', 'http://developer.berlios.de/mail/?group_id=5432', 'Mailinglist Overview'),
+    ('Project', 'http://developer.berlios.de/projects/louie/', 'Project at Berlios.de'),
+    ('Credits', 'credits.html', 'Credits for Projects, Developers etc.')        
+    ]
